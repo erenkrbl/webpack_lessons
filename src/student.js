6d1dc1661d3d3aa1010a4bc052224ba4c410ae94
Joseph Abraham <ekurubal24@gmail.com>
2021-05-01 22:41:05 +0300

    Added jsonplaceholder

--- src/student.js
+++ src/student.js
@@ -1,8 +1,11 @@
+import axios from 'axios';
+
 export default class Student{
     constructor(name){
         this.name = name;
     }
     sayName(){
-        console.log(`My name is ${this.name}`)
+        console.log(`My name is ${this.name}`);
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(response => console.log(response.data))
     }
 }
